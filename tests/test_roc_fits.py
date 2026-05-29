a5c7175fdd03c6269ab6708b7c63db3c79b3757e
l <56237933+lcdunne@users.noreply.github.com>
2026-05-29 08:20:20 +0100

    Add test with Koen data

--- tests/test_roc_fits.py
+++ tests/test_roc_fits.py
@@ -2,7 +2,7 @@ import pytest
 
 from kriterion.data import ROCData
 from kriterion.fit import fit
-from kriterion.models import SignalDetection
+from kriterion.models import SignalDetection, UnequalSignalDetection
 
 
 def test_evsdt_dunn2011():
@@ -22,3 +22,26 @@ def test_evsdt_dunn2011():
     assert model.d == pytest.approx(1.37, abs=0.05)
     assert model.n_params == 6
     assert model.dof == 4
+
+
+def test_uvsdt_koen():
+    # From Koen's ROC Toolbox, Example 1
+    data = ROCData(
+        signal=[338, 100, 117, 103, 89, 75, 67, 46, 28, 37],
+        noise=[54, 58, 103, 118, 153, 154, 121, 104, 63, 72],
+    )
+
+    model = UnequalSignalDetection(data)
+    result = fit(model)
+
+    assert result.dof == 7
+    assert result.log_likelihood == pytest.approx(-4276.8, abs=0.5)
+    assert result.g2 == pytest.approx(2.8042, abs=0.05)
+
+    assert result.aic == pytest.approx(8575.6, abs=1.0)
+    assert result.bic == pytest.approx(8637.2, abs=1.0)
+
+    assert model.d == pytest.approx(0.9958, abs=0.01)
+    assert model.signal_sd == pytest.approx(1.4055, abs=0.01)
+    assert model.n_params == 11
+    assert model.dof == 7
